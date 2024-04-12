d8b3b23d754fd79b6fb27bef29ea59b0f428d391
Maya Mistry <157775725+mmistryy@users.noreply.github.com>
2024-04-12 14:57:31 -0400

    Update ruthenium_quizgame.py

--- ruthenium_quizgame.py
+++ ruthenium_quizgame.py
@@ -5,4 +5,4 @@
 # create a trivia/quiz game with three game modes;
 # one where the game gives the user 3 clues to guess a thing or person
 # one where the game chooses a random riddle for the user to answer with 3 tries 
-
+# one basic trivia questions mode
