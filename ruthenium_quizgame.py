a03eacabf96e6290a4c43cde41bf419c212f826d
Maya Mistry <157775725+mmistryy@users.noreply.github.com>
2024-04-12 14:32:21 -0400

    Add files via upload

---
+++ ruthenium_quizgame.py
@@ -0,0 +1,8 @@
+# Group: Ruthenium 
+# Maya Mistry, Evan Schneider, Hadiya Mohammed 
+# Final Project
+
+# create a trivia/quiz game with three game modes;
+# one where the game gives the user 3 clues to guess a thing or person
+# one where the game chooses a random riddle for the user to answer with 3 tries 
+
